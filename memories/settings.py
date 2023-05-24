d843bb9fd2c6906022941210f7aa76730380b4c5
minow3 <minow3@gmail.com>
2023-05-24 14:44:12 +0100

    change in directories

--- memories/settings.py
+++ memories/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
 if os.path.isfile("env.py"):
     import env
 
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'memories.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': [TEMPLATES_DIR],
         'OPTIONS': {
             'context_processors': [
